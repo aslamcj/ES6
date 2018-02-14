57a8cdb65136d03ee805007312e83198811f8890
Aslam <aslamcj@gmail.com>
2018-02-14 16:22:25 +0530

    Create example1.js

---
+++ InbuiltHelperMethods/example1.js
@@ -0,0 +1,11 @@
+String methods
+Var testString = "Static method which doesn’t require instantiation"
+testString.startsWith("");
+testString.endsWith("");
+testString.includes("");
+testString.repeat();
+
+Number.isFinite(Infinity); 
+Number.isFinite(3); 
+Number.isSafeInteger();
+Number.isInteger(222);
